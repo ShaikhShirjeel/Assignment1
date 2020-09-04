3f289656d43327c70886d6925ab753da7397a5fd
ShaikhShirjeel <shaikh.m.shirjeel1@gmail.com>
2020-09-04 23:40:20 +0500

    comiited

--- mod2sol/js/script.js
+++ mod2sol/js/script.js
@@ -1,4 +1,4 @@
-var names = ["shirjeel","jamali","hamza","jamil","shaikh"]
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
 
 for (var i in names)
 {
